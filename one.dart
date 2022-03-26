42e38ee53b7c61015c410a68706d9817658ebd3d
Amit Srivastava <amit.srivastava4all@gmail.com>
2022-03-26 17:52:10 +0530

    Add files via upload

--- one.dart
+++ one.dart
@@ -1,18 +1,49 @@
-void main(){
-  // Every Type in Dart is an object (States).
-  int? a ; // it can be null
- // int a ;// It never be null (no Reference)
-  print(a);
-  print(a.runtimeType);
-  print("Hello Dart");
-  a = 100;
-  print(a.runtimeType);
-  String name = "AMit";
-  print(name.runtimeType);
-  double r = 90.20;
-  print(r.runtimeType);
-  bool? t = true;
-  print(t.runtimeType);
-  t = null;
+import 'dart:io';
 
+/// this a doc
+void main() {
+  print("Enter the Age");
+  int age = int.parse(stdin.readLineSync().toString());
+  // this is a single comment
+  /*
+  Multi line
+  */
+  print(10 > 20 ? 'Yes' : 'No');
+  List<int> aa = [10, 20];
+  List<int> bb = [90, 100];
+  List<int?>? ccc;
+  List<List<int>> rr = [aa, bb];
+  List<int?> cc = [...aa, ...bb, ...?ccc];
+  // Null Aware Operator
+  List<int> list = [10, 20];
+  list..add(100)..add(200)..add(300); // Cascade Notation
+  //list.add(200);
+  String m = "Hi";
+  m.toUpperCase().substring(1).toLowerCase(); // Chaining
+  String? q;
+  print(q ?? 'Hi'); // Null Aware
+  String? a1;
+  //a1 ??= 'Hi'; // Null Aware Assignment
+  print(a1?.toUpperCase()); // Conditional Access
+  //int w1 = (10 / 2).toInt();
+  int w1 = 10 ~/ 2;
+  //int w2 = (10 / 2) as int;
+  print(w2);
+  var x = "Amit";
+  String y = "Amit";
+  const z = "Amit";
+  //z = "Ram"; // Error
+  // const - Compile time constant
+  const double PI = 3.14;
+  final int MAX = 100;
+  final StringBuffer sb = new StringBuffer();
+  sb.write("Hello");
+  sb.write("HI");
+  sb.write("Ok");
+  // final - run time constant
+  //sb = StringBuffer();
+  final a = [10, 20, 30];
+  a.add(1000);
+  print(a);
+  // const StringBuffer sb2 = StringBuffer();
 }
